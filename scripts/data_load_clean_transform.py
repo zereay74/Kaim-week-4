0ef2e37b3157f62782d46e137c6081dfc46ba3c5
zereay74 <zereaymulugeta741@gmail.com>
2025-01-10 12:58:27 +0300

    data loder update

--- scripts/data_load_clean_transform.py
+++ scripts/data_load_clean_transform.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pandas as pd
 import psycopg2 
-from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 import numpy as np
 from scipy.stats import zscore
@@ -57,90 +57,7 @@ class DataLoader:
 # df = loader.load_data()
 
 
-# Load environment variables
-load_dotenv()
 
-# Fetch database connection parameters
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-class LoadSqlData:
-    """
-    A class to load SQL data from PostgreSQL using psycopg2 or SQLAlchemy.
-    """
-    
-    def __init__(self, query):
-        self.query = query
-
-    def load_data_from_postgres(self):
-        """
-        Load data from PostgreSQL using psycopg2.
-
-     
-        """
-        try:
-            # Establish connection to the database
-            connection = psycopg2.connect(
-                host=DB_HOST,
-                port=DB_PORT,
-                database=DB_NAME,
-                user=DB_USER,
-                password=DB_PASSWORD
-            )
-
-            
-            # Fetch data using pandas
-            df = pd.read_sql_query(self.query, connection)
-            
-            # Close connection
-            connection.close()
-            print('Sucessfully Loaded')
-            return df
-        except Exception as e:
-            print(f"An error occurred while loading data with psycopg2: {e}")
-            print("Connection parameters:")
-            print(f"Host: {DB_HOST}, Port: {DB_PORT}, DB: {DB_NAME}, User: {DB_USER}, Password: {DB_PASSWORD}")
-            return None
-
-
-    def load_data_using_sqlalchemy(self):
-        """
-        Load data from PostgreSQL using SQLAlchemy.
-        """
-        try:
-            # Create connection string
-            connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-            
-            # Create engine
-            engine = create_engine(connection_string)
-
-            # Fetch data using pandas
-            df = pd.read_sql_query(self.query, engine)
-            print('Sucessfully Loaded')
-            return df
-        except Exception as e:
-            print(f"An error occurred while loading data with SQLAlchemy: {e}")
-            return None
-
-
-# Data clean and transformation
-
-import logging
-import pandas as pd
-import numpy as np
-from scipy.stats import zscore
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    force=True
-)
 logger = logging.getLogger(__name__)
 
 class DataCleaner:
