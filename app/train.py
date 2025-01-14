178c65307887b15b5968a61284b91b62bfa1f9b1
zereay74 <zereaymulugeta741@gmail.com>
2025-01-14 16:25:49 +0300

    task-3 and task-2 updates to save, load and predict on model

---
+++ app/train.py
@@ -0,0 +1,23 @@
+# Data Manipulation Libraries
+import pandas as pd
+import numpy as np
+# Plotting Libraries
+from matplotlib import pyplot as plt
+import seaborn as sns
+# Machine Learning Libraries
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+# Deep Learning
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+import tensorflow as tf
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.saving import register_keras_serializable
+
+
